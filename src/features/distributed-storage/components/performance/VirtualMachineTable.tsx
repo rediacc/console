6e857c7aa1709fddbca21c7bae54e5080f5c449d
Muhammed Fatih Bayraktar <mfbayraktar@live.com>
2025-11-15 17:03:09 +0100

    fix: use named import with alias for react-window FixedSizeList
    - Change to named import FixedSizeList as List for react-window v2.x compatibility
    - Keep VirtualizedList as any to avoid version-specific type issues

--- src/features/distributed-storage/components/performance/VirtualMachineTable.tsx
+++ src/features/distributed-storage/components/performance/VirtualMachineTable.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo, useRef, useEffect } from 'react'
-import ReactWindow from 'react-window'
+import { FixedSizeList as List } from 'react-window'
 import * as InfiniteLoaderModule from 'react-window-infinite-loader'
 import { Checkbox, Space, Spin } from 'antd'
 import { Machine } from '@/types'
@@ -7,8 +7,6 @@ import MachineAssignmentStatusCell from '@/components/resources/MachineAssignmen
 import { useMachineSelection } from '@/store/distributedStorage/hooks'
 import { useTableStyles } from '@/hooks/useComponentStyles'
 import styles from './VirtualMachineTable.module.css'
-
-const List = ReactWindow.FixedSizeList
 
 type VirtualizedList = any
 type ListChildProps<T = any> = { index: number; style: React.CSSProperties; data: T }
