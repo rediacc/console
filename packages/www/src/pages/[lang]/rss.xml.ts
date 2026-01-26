b2a6fc50892e2e5784b9adeea3aa1f89f5ebb2ba
Muhammed Fatih Bayraktar <mfbayraktar@live.com>
2026-01-26 13:30:53 +0100

    style(www): fix import order in RSS feed files

--- packages/www/src/pages/[lang]/rss.xml.ts
+++ packages/www/src/pages/[lang]/rss.xml.ts
@@ -2,9 +2,9 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_URL } from '../../config/constants';
 import { SUPPORTED_LANGUAGES, getLanguageName } from '../../i18n/language-utils';
+import { getBaseSlug } from '../../utils/slug';
 import type { Language } from '../../i18n/types';
 import type { APIContext } from 'astro';
-import { getBaseSlug } from '../../utils/slug';
 
 export function getStaticPaths() {
   return SUPPORTED_LANGUAGES.map((lang) => ({ params: { lang } }));
--- packages/www/src/pages/rss.xml.ts
+++ packages/www/src/pages/rss.xml.ts
@@ -1,8 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_URL } from '../config/constants';
-import type { APIContext } from 'astro';
 import { getBaseSlug } from '../utils/slug';
+import type { APIContext } from 'astro';
 
 export async function GET(context: APIContext) {
   const blog = await getCollection('blog');
