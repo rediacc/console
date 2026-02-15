2a626e418030579b0ef94ff2771f9918f908b736
Muhammed Fatih Bayraktar <mfbayraktar@live.com>
2026-02-15 13:46:22 +0100

    chore(types): auto-regenerate renet bridge types
    Automatically regenerated by CI.

--- packages/shared/src/queue-vault/data/functions.generated.ts
+++ packages/shared/src/queue-vault/data/functions.generated.ts
@@ -518,8 +518,6 @@ export interface SetupParams {
   datastoreSize?: string;
   /** Installation source */
   from?: string;
-  /** Rclone installation source */
-  rcloneSource?: string;
   /** Docker installation source */
   dockerSource?: string;
   /** Install AMD driver */
@@ -589,7 +587,7 @@ export const BRIDGE_FUNCTIONS = [
   'setup',
 ] as const;
 
-export const BRIDGE_FUNCTIONS_VERSION = 'v0.4.91-4-g1978a25a';
+export const BRIDGE_FUNCTIONS_VERSION = 'v0.4.91-5-gdcf6a835';
 
 export type BridgeFunctionName = (typeof BRIDGE_FUNCTIONS)[number];
 
@@ -2087,14 +2085,6 @@ export const FUNCTION_DEFINITIONS: Record<BridgeFunctionName, FunctionDefinition
         options: ['apt-repo', 'tar-static', 'deb-local'],
         ui: 'dropdown',
         enum: ['apt-repo', 'tar-static', 'deb-local'],
-      },
-      rcloneSource: {
-        type: 'string',
-        default: 'install-script',
-        help: 'Rclone installation source',
-        options: ['install-script', 'package-manager', 'manual'],
-        ui: 'dropdown',
-        enum: ['install-script', 'package-manager', 'manual'],
       },
       dockerSource: {
         type: 'string',
